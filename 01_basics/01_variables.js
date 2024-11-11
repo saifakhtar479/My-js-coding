a72454a13ac3c741b0ef2d74d280ee238e2f9002
saifakhtar479 <akhtarsaif988@gmail.com>
2024-11-11 14:44:59 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountid="142325"
+let accountname="saifAkhtar"
+var accountpassword='saif@123'
+// accountid="65758"
+
+accountname="faizAkhtar"
+accountpassword="Faiz@123"
+
+
+
+
+console.log(accountid);
+console.table([accountid,accountname,accountpassword])
